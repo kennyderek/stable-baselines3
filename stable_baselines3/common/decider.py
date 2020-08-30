e8b020606effc903d94c6ca96b02b433bb527471
Kenny Derek <pickypulsars@gmail.com>
2020-08-30 09:53:42 -0400

    Add decider module for context prediction.

---
+++ stable_baselines3/common/decider.py
@@ -0,0 +1,24 @@
+
+import torch.nn as nn
+
+class Decider(nn.Module):
+
+    def __init__(self):
+        super(Decider, self).__init__()
+
+        self.batch_size = 10 # 10 trajectories
+        self.rnn = nn.LSTM(input_size=2, hidden_size=64).double()
+        self.fc1 = nn.Linear(64, 3).double() # 3 is size of the context
+    
+    def forward(self, sequence):
+        '''
+        squence: (seq_len, batch, input_size)
+        return: output, (h_n, c_n)
+                where output is (seq_len, batch, num_directions * hidden_size)
+        '''
+
+        output, hiddens = self.rnn(sequence) # returns a packed_padded_sequence
+        unpacked, unpacked_len = nn.utils.rnn.pad_packed_sequence(output) # (seq_len, batch, num_directions * hidden_size), (h_n, c_n)
+        logits = self.fc1(unpacked[-1].double()) # (batch, 3)
+        return logits
+
